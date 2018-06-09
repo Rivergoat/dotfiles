aa132f972185a8929f315650ab2bb0e8ce525f04
rivergoat <pelle.haenel@outlook.de>
2018-06-09 18:58:12 +0000

    Added new rice files:

---
+++ per.sh
@@ -0,0 +1,12 @@
+clear
+echo "Changing terminal color to white for 5 seconds... \n"
+cp ~/.config/termite/config ~/repos/dotfiles/termcache
+
+cp ~/repos/dotfiles/perunterm ~/.config/termite/config
+
+sleep 5
+
+cp ~/repos/dotfiles/termcache ~/.config/termite/config
+
+clear
+
